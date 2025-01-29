7df18a44339c21a5e95e7a252b7af2b4de0ff627
dfgdgdge <gelenadom@gmail.com>
2025-01-29 11:59:44 +0300

    монстры бегут к замку

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from intro_end import IntroScreen, GameOverScreen
 from monsters import Sceleton, Eye, Goblin, Mushroom
+from hero import Hero
 
 pygame.init()
 
@@ -44,106 +45,12 @@ class MapLoader:
         ]
         return all(0 <= cy < rows and 0 <= cx < cols and self.map_data[cy][cx] == "L" for cx, cy in corners)
 
-
-class Hero:
-    def __init__(self):
-        self.stand_sprite = pygame.image.load("hero_assets/hero.png")
-        self.image = self.stand_sprite
-        self.rect = self.image.get_rect()
-        self.frame = 0
-        self.is_r_attacking = False  # Флаг атаки
-        self.is_l_attacking = False
-        self.moving_direction = None  # Направление движения
-        self.load_animations()
-
-    def load_animations(self):
-        img_run_d = pygame.image.load("hero_assets/_Run.png")
-        img_run_a = pygame.image.load("hero_assets/inv_Run.png")
-        img_run_ws = pygame.image.load("hero_assets/_CrouchAll.png")
-        img_attack = pygame.image.load("hero_assets/_Attack.png")
-        img_l_attack = pygame.image.load("hero_assets/inv_Attack.png")
-
-        self.anim_run_d = [img_run_d.subsurface(x, 42, 28, 38) for x in range(42, 1124, 121)]
-        self.anim_run_a = [img_run_a.subsurface(x, 42, 28, 38) for x in reversed(range(42, 1124, 121))]
-        self.anim_run_ws = [img_run_ws.subsurface(x, 46, 31, 33) for x in range(42, 282, 120)]
-
-        self.anim_r_attack = [img_attack.subsurface(36, 43, 40, 37),
-                              img_attack.subsurface(171, 37, 66, 43),
-                              img_attack.subsurface(292, 45, 67, 35),
-                              img_attack.subsurface(414, 43, 58, 36)]
-
-        self.anim_l_attack = [img_l_attack.subsurface(404, 42, 39, 38),
-                              img_l_attack.subsurface(242, 37, 67, 43),
-                              img_l_attack.subsurface(122, 43, 66, 37),
-                              img_l_attack.subsurface(7, 46, 62, 34)]
-
-    def move(self, map_loader, keys):
-        if self.is_r_attacking or self.is_l_attacking:
-            return  # Во время атаки герой не двигается
-
-        new_x, new_y = self.rect.x, self.rect.y
-        self.moving_direction = None  # Сбрасываем направление
-
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            new_x -= 3
-            self.moving_direction = "left"
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            new_x += 3
-            self.moving_direction = "right"
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            new_y += 3
-            self.moving_direction = "down"
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            new_y -= 3
-            self.moving_direction = "up"
-
-        if map_loader.can_move(new_x, self.rect.y, self.rect.width, self.rect.height):
-            self.rect.x = new_x
-        if map_loader.can_move(self.rect.x, new_y, self.rect.width, self.rect.height):
-            self.rect.y = new_y
-
-    def attack_r(self):
-        """Запускает анимацию атаки."""
-        if not self.is_r_attacking:
-            self.is_r_attacking = True
-            self.frame = 0
-
-    def attack_l(self):
-        """Запускает анимацию атаки."""
-        if not self.is_l_attacking:
-            self.is_l_attacking = True
-            self.frame = 0
-
-    def animate(self):
-        """Анимация персонажа."""
-        if self.is_r_attacking:
-            if self.frame < len(self.anim_r_attack) - 1:
-                self.frame += 0.2
-            else:
-                self.is_r_attacking = False
-            self.image = self.anim_r_attack[int(self.frame)]
-            return
-
-        if self.is_l_attacking:
-            if self.frame < len(self.anim_l_attack) - 1:
-                self.frame += 0.2
-            else:
-                self.is_l_attacking = False
-            self.image = self.anim_l_attack[int(self.frame)]
-            return
-
-
-        if self.moving_direction == "right":
-            self.frame = (self.frame + 0.1) % len(self.anim_run_d)
-            self.image = self.anim_run_d[int(self.frame)]
-        elif self.moving_direction == "left":
-            self.frame = (self.frame + 0.1) % len(self.anim_run_a)
-            self.image = self.anim_run_a[int(self.frame)]
-        elif self.moving_direction in ["up", "down"]:
-            self.frame = (self.frame + 0.1) % len(self.anim_run_ws)
-            self.image = self.anim_run_ws[int(self.frame)]
-        else:
-            self.image = self.stand_sprite  # Если не двигается — стоит на месте
+    def is_wall(self, x, y):
+        """ Проверяет, является ли клетка стеной 'S' """
+        cell_x, cell_y = x // CELL_SIZE, y // CELL_SIZE
+        if 0 <= cell_y < len(self.map_data) and 0 <= cell_x < len(self.map_data[0]):
+            return self.map_data[cell_y][cell_x] == "S"
+        return False
 
 
 class Game:
@@ -159,10 +66,12 @@ class Game:
         self.clock = pygame.time.Clock()
         self.init_hero_position()
 
-        self.monster1 = Sceleton(self.screen_width, self.screen_height)
-        self.monster2 = Eye(self.screen_width, self.screen_height)
-        self.monster3 = Goblin(self.screen_width, self.screen_height)
-        self.monster4 = Mushroom(self.screen_width, self.screen_height)
+        self.monsters = [
+            Sceleton(self.screen_width, self.screen_height, self.map_loader),
+            Eye(self.screen_width, self.screen_height, self.map_loader),
+            Goblin(self.screen_width, self.screen_height, self.map_loader),
+            Mushroom(self.screen_width, self.screen_height, self.map_loader),
+        ]
 
     def init_hero_position(self):
         for y, row in enumerate(self.map_loader.map_data):
@@ -186,14 +95,18 @@ class Game:
             self.hero.move(self.map_loader, keys)
             self.hero.animate()
 
+            # ОБНОВЛЕНИЕ МОНСТРОВ
+            for monster in self.monsters:
+                monster.update()  # Убираем передачу map_loader
+
+            # Рендеринг
             self.screen.fill((124, 172, 46))
             self.map_loader.draw_map(self.screen)
             self.screen.blit(self.castle, (self.screen_width // 1.19, self.screen_height // 2.5))
 
-            self.monster1.draw(self.screen)
-            self.monster2.draw(self.screen)
-            self.monster3.draw(self.screen)
-            self.monster4.draw(self.screen)
+            # ОТРИСОВКА МОНСТРОВ
+            for monster in self.monsters:
+                monster.draw(self.screen)
 
             self.screen.blit(self.hero.image, self.hero.rect)
 
--- monsters.py
+++ monsters.py
@@ -1,47 +1,168 @@
 import pygame
 
 
-class Sceleton(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height):
+class Monster(pygame.sprite.Sprite):
+    """Базовый класс монстра, который двигается вправо и останавливается у стены (C)."""
+    def __init__(self, image_path, x, y, speed, map_loader):
         super().__init__()
-        self.image = pygame.image.load("monsters_assets/skeleton/Idle.png").subsurface(60, 50, 46, 52)
-        self.rect = self.image.get_rect()
-        self.rect.center = (170, 280)  # Размещаем монстра в центре экрана
+        self.spritesheet = pygame.image.load(image_path)  # Загружаем весь спрайт-лист
+        self.rect = pygame.Rect(x, y, 46, 52)
+        self.speed = speed
+        self.map_loader = map_loader
+        self.frame = 0  # Индекс текущего кадра анимации
+        self.frame_speed = 0.1  # Скорость анимации
+
+        # Создаём список кадров анимации движения
+        self.anim_move = [self.spritesheet.subsurface(i * 46, 50, 46, 52) for i in range(4)]
+        self.image = self.anim_move[0]  # Первый кадр анимации
+
+    def move(self):
+        """Монстр движется вправо и проверяет столкновение с замком."""
+        next_x = self.rect.x + self.speed
+        if self.can_move(next_x, self.rect.y):  # Проверка столкновения с замком
+            self.rect.x = next_x
+            self.animate()  # Обновляем анимацию движения
+
+    def can_move(self, x, y):
+        """Проверяет, можно ли двигаться (не столкнулся ли правым боком со стеной 'C')."""
+        cell_x, cell_y = int((x + self.rect.width) // 50), int(y // 50)  # Проверяем ПРАВЫЙ бок монстра
+        if 0 <= cell_y < len(self.map_loader.map_data) and 0 <= cell_x < len(self.map_loader.map_data[0]):
+            return self.map_loader.map_data[cell_y][cell_x] != "C"  # Останавливаем монстра у стены
+        return True
+
+    def animate(self):
+        """Переключает кадры анимации движения."""
+        self.frame = (self.frame + self.frame_speed) % len(self.anim_move)
+        self.image = self.anim_move[int(self.frame)]
 
     def draw(self, screen):
+        """Отрисовывает монстра."""
         screen.blit(self.image, self.rect)
 
 
-class Eye(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height):
-        super().__init__()
-        self.image = pygame.image.load("monsters_assets/eye/Flight.png").subsurface(60, 50, 46, 52)
+class Sceleton(Monster):
+    def __init__(self, screen_width, screen_height, map_loader):
+        super().__init__("monsters_assets/skeleton/Idle.png", 170, 400, 2, map_loader)
+
+        sprite_sheet = pygame.image.load("monsters_assets/skeleton/Walk.png")
+
+        self.anim_run = [sprite_sheet.subsurface(60, 50, 47, 54),
+                         sprite_sheet.subsurface(210, 50, 47, 54),
+                         sprite_sheet.subsurface(360, 50, 47, 54),
+                         sprite_sheet.subsurface(510, 50, 47, 54),]
+
+        self.image = self.anim_run[0]
         self.rect = self.image.get_rect()
-        self.rect.center = (170, 220)  # Размещаем монстра в центре экрана
+        self.rect.topleft = (170, 400)
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        self.animation_index = 0
+        self.animation_timer = 0
+
+    def update(self):
+        self.rect.x += self.speed  # Двигаем монстра вправо
+
+        # Анимация бега
+        self.animation_timer += 1
+        if self.animation_timer >= 5:
+            self.animation_timer = 0
+            self.animation_index = (self.animation_index + 1) % len(self.anim_run)
+            self.image = self.anim_run[self.animation_index]
 
 
-class Goblin(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height):
-        super().__init__()
-        self.image = pygame.image.load("monsters_assets/goblin/Idle.png").subsurface(60, 50, 46, 52)
+class Eye(Monster):
+    def __init__(self, screen_width, screen_height, map_loader):
+        super().__init__("monsters_assets/eye/Flight.png", 170, 400, 2, map_loader)
+
+        sprite_sheet = pygame.image.load("monsters_assets/eye/Flight.png")
+
+        self.anim_run = [sprite_sheet.subsurface(55, 60, 45, 30),
+                         sprite_sheet.subsurface(205, 60, 45, 30),
+                         sprite_sheet.subsurface(360, 60, 45, 30),
+                         sprite_sheet.subsurface(505, 60, 45, 30),
+                         sprite_sheet.subsurface(655, 60, 45, 30),
+                         sprite_sheet.subsurface(805, 60, 45, 30),
+                         sprite_sheet.subsurface(955, 60, 45, 30),
+                         sprite_sheet.subsurface(1105, 60, 45, 30),]
+
+        self.image = self.anim_run[0]
         self.rect = self.image.get_rect()
-        self.rect.center = (170, 340)  # Размещаем монстра в центре экрана
+        self.rect.topleft = (170, 200)
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        self.animation_index = 0
+        self.animation_timer = 0
+
+    def update(self):
+        self.rect.x += self.speed  # Двигаем монстра вправо
+
+        # Анимация бега
+        self.animation_timer += 1
+        if self.animation_timer >= 5:
+            self.animation_timer = 0
+            self.animation_index = (self.animation_index + 1) % len(self.anim_run)
+            self.image = self.anim_run[self.animation_index]
 
 
-class Mushroom(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height):
-        super().__init__()
-        self.image = pygame.image.load("monsters_assets/mushroom/Idle.png").subsurface(60, 50, 46, 52)
+class Goblin(Monster):
+    def __init__(self, screen_width, screen_height, map_loader):
+        super().__init__("monsters_assets/goblin/Idle.png", 170, 400, 2, map_loader)
+
+        sprite_sheet = pygame.image.load("monsters_assets/goblin/Run.png")
+
+        self.anim_run = [sprite_sheet.subsurface(50, 60, 42, 42),
+                         sprite_sheet.subsurface(205, 60, 42, 42),
+                         sprite_sheet.subsurface(360, 60, 42, 42),
+                         sprite_sheet.subsurface(510, 60, 42, 42),
+                         sprite_sheet.subsurface(660, 60, 42, 42),
+                         sprite_sheet.subsurface(810, 60, 42, 42),
+                         sprite_sheet.subsurface(960, 60, 42, 42),
+                         sprite_sheet.subsurface(1110, 60, 42, 42),]
+
+        self.image = self.anim_run[0]
         self.rect = self.image.get_rect()
-        self.rect.center = (170, 400)  # Размещаем монстра в центре экрана
+        self.rect.topleft = (170, 250)
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        self.animation_index = 0
+        self.animation_timer = 0
+
+    def update(self):
+        self.rect.x += self.speed  # Двигаем монстра вправо
+
+        # Анимация бега
+        self.animation_timer += 1
+        if self.animation_timer >= 5:
+            self.animation_timer = 0
+            self.animation_index = (self.animation_index + 1) % len(self.anim_run)
+            self.image = self.anim_run[self.animation_index]
 
 
+class Mushroom(Monster):
+    def __init__(self, screen_width, screen_height, map_loader):
+        super().__init__("monsters_assets/mushroom/Idle.png", 170, 400, 2, map_loader)
+
+        sprite_sheet = pygame.image.load("monsters_assets/mushroom/Run.png")
+
+        self.anim_run = [sprite_sheet.subsurface(60, 60, 30, 40),
+                         sprite_sheet.subsurface(210, 60, 30, 40),
+                         sprite_sheet.subsurface(360, 60, 30, 40),
+                         sprite_sheet.subsurface(510, 60, 30, 40),
+                         sprite_sheet.subsurface(660, 60, 30, 40),
+                         sprite_sheet.subsurface(810, 60, 30, 40),
+                         sprite_sheet.subsurface(960, 60, 30, 40),
+                         sprite_sheet.subsurface(1110, 60, 30, 40),]
+
+        self.image = self.anim_run[0]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (170, 330)
+
+        self.animation_index = 0
+        self.animation_timer = 0
+
+    def update(self):
+        self.rect.x += self.speed  # Двигаем монстра вправо
+
+        # Анимация бега
+        self.animation_timer += 1
+        if self.animation_timer >= 5:
+            self.animation_timer = 0
+            self.animation_index = (self.animation_index + 1) % len(self.anim_run)
+            self.image = self.anim_run[self.animation_index]
